288350aac5433aafa6d75af4c60547936a4f937b
keyne <keyne.pekar@uphf.fr>
2024-11-19 19:44:58 +0100

    ajout du soleil et de nuages

--- src/main.c
+++ src/main.c
@@ -24,10 +24,26 @@ int main() {
     RVB couleurBas = {253, 208, 134};
     fond_degrade_rectangle(im, p1, p2, couleurHaut, couleurBas);
 
+    // Soleil
+    dessiner_soleil(im, (COORD){825, 450}, 40, 100, (RVB){250, 221, 186});
+
     // Montagnes
-    dessiner_et_remplir_forme(im, "./assets/Montagne3.txt", (RVB){0, 0, 0}, (RVB){125, 175, 179}, 0, 120);
-    dessiner_et_remplir_forme(im, "./assets/Montagne2.txt", (RVB){0, 0, 0}, (RVB){99, 159, 167}, 0, 120);
-    dessiner_et_remplir_forme(im, "./assets/Montagne1.txt", (RVB){0, 0, 0}, (RVB){67, 114, 119}, 0, 120);
+    dessiner_et_remplir_forme(im, "./assets/Montagne3.txt", (RVB){125, 175, 179}, (RVB){125, 175, 179}, 0, 150);
+    dessiner_et_remplir_forme(im, "./assets/Montagne2.txt", (RVB){99, 159, 167}, (RVB){99, 159, 167}, -1, 150);
+    dessiner_et_remplir_forme(im, "./assets/Montagne1.txt", (RVB){67, 114, 119}, (RVB){67, 114, 119}, 0, 150);
+
+    // Nuages
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Long1.txt", (RVB){177, 189, 153}, (RVB){177, 189, 153}, 0, 0);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Long2.txt", (RVB){198, 195, 148}, (RVB){198, 195, 148}, 0, 0);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond1.1.txt", (RVB){253, 208, 138}, (RVB){253, 208, 138}, 0, 0);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond1.2.txt", (RVB){242, 191, 125}, (RVB){242, 191, 125}, 0, 0);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond2.1.txt", (RVB){253, 208, 138}, (RVB){253, 208, 138}, 0, 275);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond2.2.txt", (RVB){242, 191, 125}, (RVB){242, 191, 125}, 0, 275);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond3.1.txt", (RVB){253, 208, 138}, (RVB){253, 208, 138}, 0, 175);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond3.2.txt", (RVB){242, 191, 125}, (RVB){242, 191, 125}, 0, 216);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond4.1.txt", (RVB){253, 208, 138}, (RVB){253, 208, 138}, 0, 0);
+    dessiner_et_remplir_forme(im, "./assets/Nuage-Rond4.2.txt", (RVB){242, 191, 125}, (RVB){242, 191, 125}, 0, 0);
+
     ecrire_corps(im, df);
 
     liberer_buffer(im);
